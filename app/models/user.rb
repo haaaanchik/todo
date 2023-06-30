4a1a46dcd7ab671b4d180f4b1a44ae9c5bb5705a
Khangerey Saipudinov <saypudinov05@gmail.com>
2023-06-30 19:23:19 +0300

    Changing the login form and code refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, length: { minimum: 6, maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 50 }
 end
